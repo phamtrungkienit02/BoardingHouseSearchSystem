499b261811e3e5f2fbf1d6cbc6243826b7b0dff2
phamtrungkienit02 <2051052067kien@ou.edu.vn>
2024-06-02 11:21:13 +0700

    edit pagination (1 -> 10)

--- backend/boardingHouseApp/paginators.py
+++ backend/boardingHouseApp/paginators.py
@@ -2,8 +2,8 @@ from rest_framework import pagination
 
 
 class RoomPaginator(pagination.PageNumberPagination):
-    page_size = 1
+    page_size = 10
 
 
 class CommentPaginator(pagination.PageNumberPagination):
-    page_size = 2
+    page_size = 10
