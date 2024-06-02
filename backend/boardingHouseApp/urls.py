60994451af0d72c8773d7d4f88b3ba69b138535b
phamtrungkienit02 <2051052067kien@ou.edu.vn>
2024-06-02 11:16:14 +0700

    new url: /owners/

--- backend/boardingHouseApp/urls.py
+++ backend/boardingHouseApp/urls.py
@@ -1,18 +1,19 @@
-import pathlib
+# import pathlib
 
 from django.urls import path, include
 from rest_framework import routers
 
 from . import views
 
+
 router = routers.DefaultRouter()
 router.register('users', views.UserViewSet, basename='users')
 router.register('categories', views.CategoryViewSet, basename='categories')
 router.register('rooms', views.RoomViewSet, basename='rooms')
+router.register('owners', views.OwnerViewSet, basename='owners')
 router.register('posts', views.PostViewSet, basename='posts')
 router.register('room-comment', views.CommentRoomViewSet, basename='room-comment')
 router.register('post-comment', views.CommentPostViewSet, basename='post-comment')
-
 
 urlpatterns = [
     path('', include(router.urls))
