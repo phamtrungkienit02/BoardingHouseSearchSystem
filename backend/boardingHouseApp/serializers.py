08a0b3d33a299789088ed43ae5da756ccfceddaa
phamtrungkienit02 <2051052067kien@ou.edu.vn>
2024-06-02 11:22:34 +0700

    new serializer (roomDetail, postDetail, owner) edit serializer (room, post)

--- backend/boardingHouseApp/serializers.py
+++ backend/boardingHouseApp/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
-from .models import User, Category, Room, Post, ImageRoom, PostComment
+from .models import User, Category, Room, Post, ImageRoom, PostComment, District
 from urllib.parse import urljoin
+from cloudinary.models import CloudinaryField
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+class DistrictSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = District
+        fields = ['name']
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -55,33 +62,65 @@ class ImageSerializer(serializers.ModelSerializer):
         # fields = '__all__'
 
 
+# class BaseImageSerializer(serializers.ModelSerializer):
+#     images = serializers.SerializerMethodField('get_images')
+#
+#     def get_images(self, obj):
+#         return ImageSerializer(obj.imageroom_set.all(), many=True).data
+
+
 class RoomSerializer(serializers.ModelSerializer):
-    # images = ImageSerializer(many=True)
+
+    district = DistrictSerializer()
+    # user = AuthenticatedUserSerializer()
+    user = UserSerializer()
     images = serializers.SerializerMethodField('get_images')
-    user = AuthenticatedUserSerializer()
+
+    def get_images(self, obj):
+        return ImageSerializer(obj.imageroom_set.all(), many=True).data
 
     class Meta:
         model = Room
-        # fields = ['id', 'title', 'description', 'created_date', 'updated_date', 'name',
-        #           'price', 'num_of_people', 'area', 'address', 'longitude', 'latitude', 'category', 'image']
-        fields = ['id', 'title', 'description', 'images', 'user']
+        # fields = ['id', 'title', 'description', 'price', 'user', 'category']
+        fields = ['id', 'title', 'user', 'images', 'district', 'created_date', 'updated_date', 'price']
+
+
+class RoomDetailSerializer(RoomSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = RoomSerializer.Meta.model
+        fields = RoomSerializer.Meta.fields + ['name', 'num_of_people', 'area', 'address',
+                                               'longitude', 'latitude', 'category']
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    user = UserSerializer()
+
+    images = serializers.SerializerMethodField('get_images')
+
+    class Meta:
+        model = Room
+        fields = ['id', 'name', 'address', 'images', 'user', 'longitude', 'latitude', 'category']
 
     def get_images(self, obj):
         return ImageSerializer(obj.imageroom_set.all(), many=True).data
 
 
-class RoomDetailSerializer(serializers.ModelSerializer):
-    pass
-
-    # class Meta:
-    #     model = RoomSerializer.Meta.model
-    #     fields = RoomSerializer.Meta.fields + ['image']
-
-
 class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    district = DistrictSerializer()
+
     class Meta:
         model = Post
-        fields = ['id', 'title', 'description', 'created_date', 'updated_date']
+        fields = ['id', 'title', 'created_date', 'updated_date', 'user', 'district']
+
+
+class PostDetailSerializer(PostSerializer):
+    class Meta:
+        model = PostSerializer.Meta.model
+        fields = PostSerializer.Meta.fields + ['description']
 
 
 class CommentSerializer(serializers.ModelSerializer):
