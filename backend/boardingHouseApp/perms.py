62e53828c794eb64831e8c79d236fdf443fc6777
phamtrungkienit02 <2051052067kien@ou.edu.vn>
2024-06-02 11:15:10 +0700

    update permission(owner)

--- backend/boardingHouseApp/perms.py
+++ backend/boardingHouseApp/perms.py
@@ -7,7 +7,18 @@ class CommentOwner(permissions.IsAuthenticated):
         return super().has_permission(request, view) and request.user == instance.user
 
 
-class AdminPerm(permissions.IsAuthenticated):
-    def has_object_permission(self, request, view, instance):
-        return super().has_permission(request, view) and request.user.is_superuser is True
+# class AdminPerm(permissions.IsAuthenticated):
+#     def has_object_permission(self, request, view, instance):
+#         # return super().has_permission(request, view) and request.user.is_superuser is True
+#         return bool(request.user and request.user.is_superuser)
+
+# cho cả admin và chủ trọ được đăng phòng trọ
+class OwnerPerm(permissions.IsAuthenticated):
+    # def has_object_permission(self, request, view, instance):
+        #return super().has_permission(request, view) and request.user.role == 'OWNER'
+    def has_permission(self, request, view):
+        return super().has_permission(request, view) and (request.user.role in ['OWNER']
+                                                          or request.user.is_superuser is True)
+
+
 
