d45d76b613aa60271d45eb7795609272f8fb9918
phamtrungkienit02 <2051052067kien@ou.edu.vn>
2024-06-22 13:57:33 +0700

    profile user

---
+++ frontend/BoardingHouseMobile/components/Users/Profile.js
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { View, Text, Image } from "react-native";
+import { Button } from "react-native-paper";
+import { MyDispatchContext, MyUserContext } from "../../configs/Context";
+import MyStyles from "../../styles/MyStyles";
+
+const Profile = () => {
+    const user = useContext(MyUserContext);
+    const dispatch = useContext(MyDispatchContext);
+
+    return (
+        <View style={MyStyles.container}>
+            <Text style={MyStyles.subject}>THÔNG TIN NGƯỜI DÙNG</Text>
+            <Image source={{uri: user.avatar }} style={[MyStyles.avatar, MyStyles.margin]} />
+            <Text style={MyStyles.subject}>Chào {user.username}!</Text>
+            <Button icon="logout" onPress={() => dispatch({"type": "logout"})}>ĐĂNG XUẤT</Button>
+        </View>
+    );
+}
+
+export default Profile;
